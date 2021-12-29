116f007d272c7a2cc6497aa0578782c713418739
zsofi <zsofiath@gmail.com>
2021-12-29 15:30:02 +0100

    :sparkles: show items

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,43 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { artistsListAll } from "../store/actions";
+import { Link } from "react-router-dom";
 
 class Home extends Component {
   componentDidMount() {
     this.props.dispatch(artistsListAll());
   }
 
+  showArtistAll = (data) => data?.artistList?.map((item) => {
+          return (
+            <Link
+              to={`/artist/${item.id}`}
+              key={item.id}
+              className="artist_item"
+            >
+              <div
+                className="cover"
+                style={{ background: `url(/images/${item.cover})` }}
+              >
+                <div>{item.name}</div>
+              </div>
+            </Link>
+          );
+        });
+
   render() {
-      console.log(this.props.artists);
-    return <div>Home</div>;
+    return (
+      <>
+        <div className="search_container">
+          <h2>Search your artist</h2>
+          <input type="text" />
+        </div>
+
+        <div className="artist_container">
+          {this.showArtistAll(this.props.artists)}
+        </div>
+      </>
+    );
   }
 }
 
