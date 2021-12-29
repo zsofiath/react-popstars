2a2954e964696e1b430e486ef0b178a6b7cf736d
zsofi <zsofiath@gmail.com>
2021-12-29 12:47:35 +0100

    :sparkles: init redux - add Provider - Provider uses middlewares used in store - store gets reducers - reducers/index.js collects every reducers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import promiseMiddleware from "redux-promise";
 import Routes from "./routes";
+import reducers from './store/reducers';
 
+const createStoreWithMiddleWare = applyMiddleware(promiseMiddleware)(createStore);
 
 ReactDOM.render(
+  <Provider store={createStoreWithMiddleWare(reducers)}>
     <BrowserRouter>
       <Routes></Routes>
-    </BrowserRouter>,
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
