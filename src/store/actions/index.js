c691c5c3b6f96027ef70c9605b2b35c4b8c71c6c
zsofi <zsofiath@gmail.com>
2022-01-02 17:42:29 +0100

    :clown_face: mock database

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -3,33 +3,36 @@ import axios from 'axios';
 const URL = 'http://localhost:3004'
 
 export function artistsListAll(){
-    const request = axios.get(`${URL}/artists?_limit=6`).then(resp => resp.data);
+    // const request = axios.get(`${URL}/artists?_limit=6`).then(resp => resp.data);
+    let json = require('../../db.json');
+    
 
     return {
         type: GET_ARTISTS_ALL,
-        payload: request
+        payload: json.artists
     }
 }
 
 export function artistsListSearch(keywords){
-    const request = axios.get(`${URL}/artists?q=${keywords}&&_limit=6`).then(resp => resp.data);
-
+    // const request = axios.get(`${URL}/artists?q=${keywords}&&_limit=6`).then(resp => resp.data);
+    let json = require('../../db.json');
+    
     return {
         type: GET_ARTISTS_SEARCH,
-        payload: request
+        payload: json.artists.filter(artist => artist.name.includes(keywords))
     }
 }
 
 export function artistDetail(id) {
-    const request = axios.get(`${URL}/artists?id=${id}`).then(resp => resp.data);
+    //  const request = axios.get(`${URL}/artists?id=${id}`).then(resp => resp.data);
+    let json = require('../../db.json');
     return {
         type: GET_ARTISTS_DETAIL,
-        payload: request
+        payload: json.artists.filter(artist => artist.id == id)
     }
 }
 
 export function clearArtistDetail(id) {
-    const request = axios.get(`${URL}/artists?id=${id}`).then(resp => resp.data);
     return {
         type: CLEAR_ARTIST_DETAIL,
         payload: null
